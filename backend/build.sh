006f9e49637acfa4875a13247a61009d8398ee6e
NikolasVB <tivalik2@gmail.com>
2025-07-24 21:51:53 +0300

    Refactor: separate prod requirements

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r backend/requirements.txt
+pip install -r backend/requirements-prod.txt
 
 python backend/manage.py collectstatic --no-input
 python backend/manage.py migrate
