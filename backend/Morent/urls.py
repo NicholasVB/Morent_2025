15fa083e6a7f44ce3018b5409a4268d76321f3ed
NikolasVB <tivalik2@gmail.com>
2025-07-27 17:06:58 +0300

    change main urls.py

--- backend/Morent/urls.py
+++ backend/Morent/urls.py
@@ -24,6 +24,10 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    re_path(r"^(?!api/|admin/).*", TemplateView.as_view(template_name="index.html"))
+    re_path(r'^(?!api/|admin/|media/|static/).*$', TemplateView.as_view(template_name='index.html')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Добавляем это даже если DEBUG=False (но только на dev!)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
